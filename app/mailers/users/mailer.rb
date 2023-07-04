7841b8167da0833403177405081321a6a5208acb
masahisazumi <m.hisazumi@gmail.com>
2023-07-04 13:47:56 +0900

    devise 認証メール

---
+++ app/mailers/users/mailer.rb
@@ -0,0 +1,3 @@
+class Users::Mailer < Devise::Mailer
+  # カスタムメソッドをここに追加
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'pages/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users, :path => '', :path_names => {:sign_up => 'register', :sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}
+
 end
