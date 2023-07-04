e5f803430f05dfaf4d1e42c53b1887965202625c
masahisazumi <m.hisazumi@gmail.com>
2023-07-04 15:56:48 +0900

    navbar footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
 
   # root "articles#index"
-  devise_for :users, :path => '', :path_names => {:sign_up => 'register', :sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}
+  devise_for :users, skip: :all
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get 'register', to: 'devise/registrations#new', as: :new_user_registration
+    post 'register', to: 'devise/registrations#create', as: :user_registration
+    get 'profile', to: 'devise/registrations#edit', as: :edit_user_registration
+    put 'profile', to: 'devise/registrations#update'
+    patch 'profile', to: 'devise/registrations#update'
+    delete 'profile', to: 'devise/registrations#destroy'
+  end
 
 end
