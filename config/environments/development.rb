564b7233489dabaee60b4e77da9794d134d04a74
yoshihama <yuya.soccer0224@gmail.com>
2023-07-20 22:27:38 +0900

    local でメールを確認できるように設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener_web
   # config.action_mailer.smtp_settings = {
   #   address:              'mail1009.onamae.ne.jp',
   #   port:                 465,
@@ -80,13 +80,13 @@ Rails.application.configure do
   #   authentication:       'plain',
   #   enable_starttls_auto: true
   # }
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    user_name: "m.hisazumi@gmail.com",
-    password: "mxadpgfzegrgnvzz",
-    authentication: "plain",
-    enable_starttls_auto: true,
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "gmail.com",
+  #   user_name: "m.hisazumi@gmail.com",
+  #   password: "mxadpgfzegrgnvzz",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  # }
 end
