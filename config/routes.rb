e89f189f0f250f8919ceb557034890f241423e7d
Masa <m.hisazumi@gmail.com>
2023-09-19 21:32:25 +0900

    Fix asset compilation issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 
   # root "pages#home"
 
